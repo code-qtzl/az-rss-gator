3f5c26d3b2fab51e77a6b1a280e1aec3abdc2952
code-qtzl <azepeda90@outlook.com>
2025-12-08 18:26:30 -0600

    [WIP] added function to fetch post, another function to last fetched function and another for marking the fetch function.

--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -1,7 +1,7 @@
 import { db } from '..';
 import { feeds, users } from '../schema';
 import { firstOrUndefined } from './utils';
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 
 export async function createFeed(name: string, url: string, userId: string) {
 	const result = await db
@@ -26,6 +26,7 @@ export async function getAllFeeds() {
 			url: feeds.url,
 			userId: feeds.userId,
 			createdBy: users.name,
+			last_fetched_at: feeds.last_fetched_at,
 		})
 		.from(feeds)
 		.innerJoin(users, eq(feeds.userId, users.id));
@@ -37,6 +38,26 @@ export async function getFeedByUrl(url: string) {
 		.select()
 		.from(feeds)
 		.where(eq(feeds.url, url))
+		.limit(1);
+	return firstOrUndefined(result);
+}
+
+export async function markFeedFetched(feedId: string) {
+	const now = new Date();
+	await db
+		.update(feeds)
+		.set({
+			last_fetched_at: now,
+			updatedAt: now,
+		})
+		.where(eq(feeds.id, feedId));
+}
+
+export async function getNextFeedToFetch() {
+	const result = await db
+		.select()
+		.from(feeds)
+		.orderBy(sql`${feeds.last_fetched_at} NULLS FIRST`)
 		.limit(1);
 	return firstOrUndefined(result);
 }
